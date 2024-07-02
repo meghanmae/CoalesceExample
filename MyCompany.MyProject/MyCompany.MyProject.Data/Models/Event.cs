338c911487fb909f338e7a60a5aa756a5462f899
Meghan <Meg.Woodford@IntelliTect.com>
2024-07-02 12:02:17 -0700

    Add behavior

--- MyCompany.MyProject/MyCompany.MyProject.Data/Models/Event.cs
+++ MyCompany.MyProject/MyCompany.MyProject.Data/Models/Event.cs
@@ -12,4 +12,17 @@ public class Event : OrganizationBase
     [Required]
     public required DateTimeOffset Start { get; set; }
     public required DateTimeOffset End { get; set; }
+
+    public class EventBehaviors(CrudContext<AppDbContext> context) : StandardBehaviors<Event, AppDbContext>(context)
+    {
+        public override ItemResult BeforeSave(SaveKind kind, Event? oldItem, Event item)
+        {
+            if (item.End < item.Start)
+            {
+                return "End date cannot be before the start date.";
+            }
+
+            return base.BeforeSave(kind, oldItem, item);
+        }
+    }
 }
